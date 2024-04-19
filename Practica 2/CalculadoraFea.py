df7352549db45dbca0dac8da07b3ee997e6d006c
zepolcore <martinlopezutn@gmail.com>
2024-04-19 12:14:21 -0300

    Calculadora basica

--- Practica 2/CalculadoraFea.py
+++ Practica 2/CalculadoraFea.py
@@ -8,7 +8,20 @@ Crear un programa que le solicite al usuario dos numeros enteros y luego imprima
 """
 
 print("Ingrese dos numeros enteros")
-numero1 = int(input(Inrese el pimer numero: ))
+numero1 = int(input("Inrese el pimer numero: "))
 numero2 = int(input("Ingrese el segundo numero: "))
 
-#funcion dividir
+suma = numero1 + numero2
+print("el resultado de la suma es: " + str(suma))
+
+resta = numero1 - numero2
+print("el resultado de la resta es: " + str(resta))
+
+multiplicacion = numero1 * numero2
+print("el resultado de la multiplicacion es: " + str(multiplicacion))
+
+division = numero1 / numero2
+print("El resultado de la division es: " + str(int(division)))
+
+resto = numero1 / numero2
+print("El resultado del resto es: " + str(int(resto)))
